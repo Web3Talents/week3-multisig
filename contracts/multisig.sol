f7784fdc890c454746de9c5b20c8d2b4b85cec1f
Ibrahim Aziz <ibraziz21@gmail.com>
2023-02-15 13:16:32 +0300

    Create multisig.sol

---
+++ contracts/multisig.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.17;
+
+//import IERC20 interface
+import "@openzeppelin/contracts/tokens/ERC20/IERC20.sol";
+
+contract Multisig {
+    error InvalidInput();
+
+
+
+    constructor() {
+        
+    }
+
+
+}
